32ed72ea935cf9646a57b34408740e44fe0acc96
Tóth Csaba <tocsamanatee@gmail.com>
2020-12-12 16:02:34 +0100

    Created a Result Entity

---
+++ app/src/main/java/com/example/mobiledev_app/database/Result.kt
@@ -0,0 +1,20 @@
+package com.example.mobiledev_app.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "game_result_table")
+data class Result(
+    @PrimaryKey(autoGenerate = true)
+    var userId: Long = 0L,
+
+    @ColumnInfo(name = "game_date")
+    val date: Long = System.currentTimeMillis(),
+
+    @ColumnInfo(name = "user_name")
+    var userName: String,
+
+    @ColumnInfo(name = "score")
+    var score: Int = 0
+)
