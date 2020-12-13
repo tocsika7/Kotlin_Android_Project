5a964533a570d65b237ae730692f43c5734fedc4
Tóth Csaba <tocsamanatee@gmail.com>
2020-12-13 20:05:14 +0100

    Put all the Leaderboard data in the RecyclerView

--- app/src/main/java/com/example/mobiledev_app/leaderboard/ResultAdapter.kt
+++ app/src/main/java/com/example/mobiledev_app/leaderboard/ResultAdapter.kt
@@ -2,34 +2,46 @@ package com.example.mobiledev_app.leaderboard
 
 import android.view.LayoutInflater
 import android.view.TextureView
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobiledev_app.R
 import com.example.mobiledev_app.ResultItemViewHolder
+import com.example.mobiledev_app.convertLongToDateString
 import com.example.mobiledev_app.database.Result
 
-class ResultAdapter: RecyclerView.Adapter<ResultItemViewHolder>()  {
+class ResultAdapter: RecyclerView.Adapter<ResultAdapter.ViewHolder>()  {
     var data = listOf<Result>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-
-
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: ResultItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.score.toString()
+        val res = holder.itemView.context.resources
+        holder.userName.text = item.userName
+        holder.score.text = item.score.toString()
+        holder.date.text = convertLongToDateString(item.date)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResultItemViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.result_item_view, parent, false) as TextView
+    override fun onCreateViewHolder(
+        parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater =
+            LayoutInflater.from(parent.context)
+        val view = layoutInflater
+            .inflate(R.layout.result_item_view,
+                parent, false)
+        return ViewHolder(view)
+    }
 
-        return ResultItemViewHolder(view)
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val userName: TextView = itemView.findViewById(R.id.user_name)
+        val score: TextView = itemView.findViewById(R.id.score)
+        val date: TextView = itemView.findViewById(R.id.date)
     }
 
 
