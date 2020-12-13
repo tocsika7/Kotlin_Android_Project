5cbd3345c84d29052c939e986bb21b16ffdbbe30
Tóth Csaba <tocsamanatee@gmail.com>
2020-12-13 20:20:37 +0100

    Refactored code in the ResultAdapter

--- app/src/main/java/com/example/mobiledev_app/leaderboard/ResultAdapter.kt
+++ app/src/main/java/com/example/mobiledev_app/leaderboard/ResultAdapter.kt
@@ -12,6 +12,7 @@ import com.example.mobiledev_app.convertLongToDateString
 import com.example.mobiledev_app.database.Result
 
 class ResultAdapter: RecyclerView.Adapter<ResultAdapter.ViewHolder>()  {
+
     var data = listOf<Result>()
         set(value) {
             field = value
@@ -22,26 +23,37 @@ class ResultAdapter: RecyclerView.Adapter<ResultAdapter.ViewHolder>()  {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        val res = holder.itemView.context.resources
-        holder.userName.text = item.userName
-        holder.score.text = item.score.toString()
-        holder.date.text = convertLongToDateString(item.date)
+        holder.bind(item)
     }
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater =
-            LayoutInflater.from(parent.context)
-        val view = layoutInflater
-            .inflate(R.layout.result_item_view,
-                parent, false)
-        return ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
     }
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    class ViewHolder private constructor (itemView: View): RecyclerView.ViewHolder(itemView){
         val userName: TextView = itemView.findViewById(R.id.user_name)
         val score: TextView = itemView.findViewById(R.id.score)
         val date: TextView = itemView.findViewById(R.id.date)
+
+        fun bind(
+            item: Result
+        ) {
+            val res = itemView.context.resources
+            userName.text = item.userName
+            score.text = item.score.toString()
+            date.text = convertLongToDateString(item.date)
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.result_item_view, parent, false)
+
+                return ViewHolder(view)
+            }
+        }
+
     }
 
 
